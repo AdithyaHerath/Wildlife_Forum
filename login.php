dce5a758a45da12b47c7aa0a79b249c8f785f5ec
Dulieka-Himashan <d.shanz0805@gmail.com>
2025-05-18 14:40:08 +0530

    Update login.php
    Add user login with input validation, password check, and session handling

--- login.php
+++ login.php
@@ -2,6 +2,13 @@
 require_once 'includes/header.php';
 
 $error = '';
-
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = trim($_POST['username']);
+    $password = $_POST['password'];
+    
+    if (empty($username) || empty($password)) {
+        $error = "Please fill in all fields.";} 
+        else {}
+}
 
 <?php require_once 'includes/footer.php'; ?>
