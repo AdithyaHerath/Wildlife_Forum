4c6388bc51ac70922d1741ba0b5823600da5bbbe
OAhewavitharane <oshanihewavitharane@gmail.com>
2025-05-18 17:28:30 +0530

    update category.php
    Fetch details for the specified category from the database

--- category.php
+++ category.php
@@ -8,5 +8,14 @@ if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
 
 $category_id = $_GET['id'];
 
+// Get category details
+$category_sql = "SELECT * FROM categories WHERE category_id = ?";
+$stmt = $conn->prepare($category_sql);
+$stmt->execute([$category_id]);
+$category = $stmt->fetch();
 
+if (!$category) {
+    header("location: index.php");
+    exit();
+}
 ?>
