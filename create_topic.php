427cef3cbca79c4e7e550675c8e1a92d4ebbfd8c
Isuru kavisanka <isurukavisanka3@gmail.com>
2025-05-18 20:42:28 +0530

    Update create_topic.php
    Retrieve all categories from the database for the category selection dropdown

--- create_topic.php
+++ create_topic.php
@@ -10,4 +10,7 @@ $error = '';
 $success = '';
 $category_id = isset($_GET['category_id']) ? $_GET['category_id'] : '';
 
+// Get categories for dropdown
+$categories_sql = "SELECT * FROM categories ORDER BY name";
+$categories_result = $conn->query($categories_sql);
 ?>
