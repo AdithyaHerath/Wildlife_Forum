297c95c7ce995704d3a2cf7bbff7191643e3bce5
OAhewavitharane <oshanihewavitharane@gmail.com>
2025-05-18 17:22:51 +0530

    update category.php
    Include header and validate category ID parameter

---
+++ category.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'includes/header.php';
+
+if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
+    header("location: index.php");
+    exit();
+}
+
+$category_id = $_GET['id'];
+
+
+?>
