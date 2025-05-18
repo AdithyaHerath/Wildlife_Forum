1445c2f563813ef370ae455da1b1ca95a6845786
adithya herath <adithyaidura@gmail.com>
2025-05-18 22:24:23 +0530

    Refactor HTML structure for semantic clarity and consistency

--- aboutUs.php
+++ aboutUs.php
@@ -5,4 +5,15 @@
 require_once 'includes/header.php';
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+</head>
+<body>
+    
+</body>
+</html>
+
+
 <?php require_once 'includes/footer.php'; ?>
