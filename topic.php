ad48b2a780ea0f39ce0392ddfa8d3785d38eb087
dasulaNI <dasulanim23@gmail.com>
2025-05-18 21:25:56 +0530

    update topic.php
    Include header and validate the topic ID parameter

---
+++ topic.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'includes/header.php';
+
+if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
+    header("location: index.php");
+    exit();
+}
+
+$topic_id = $_GET['id'];
+$error = '';
+$success = '';
+
+?>
