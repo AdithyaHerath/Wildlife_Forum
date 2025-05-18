00b8e89f65d21e31b6276e5a6a9ac1e84ed18dcb
dasulaNI <dasulanim23@gmail.com>
2025-05-18 21:31:05 +0530

    update topic.php
    Retrieve topic details, including category and author information

--- topic.php
+++ topic.php
@@ -10,4 +10,20 @@ $topic_id = $_GET['id'];
 $error = '';
 $success = '';
 
+// Get topic details
+$topic_sql = "SELECT t.*, c.name as category_name, c.category_id, u.username 
+              FROM topics t 
+              JOIN categories c ON t.category_id = c.category_id 
+              JOIN users u ON t.user_id = u.user_id 
+              WHERE t.topic_id = ?";
+
+$stmt = $conn->prepare($topic_sql);
+$stmt->execute([$topic_id]);
+$topic = $stmt->fetch();
+
+if (!$topic) {
+    header("location: index.php");
+    exit();
+}
+
 ?>
