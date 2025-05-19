b5f961661d0c87cea89440233bf8ce608b73fb85
adithya herath <adithyaidura@gmail.com>
2025-05-19 19:57:48 +0530

    Check if user is admin

--- admin.php
+++ admin.php
@@ -1,3 +1,13 @@
-<?php include("includes/header.php"); ?>
-<h1>Admin Panel</h1>
+<?php include("includes/header.php"); 
+
+// Check if user is admin
+if (!$logged_in || !$is_admin) {
+    header("location: index.php");
+    exit();
+}
+
+$error = '';
+$success = '';
+
+?>
 <?php include("includes/footer.php"); ?>
