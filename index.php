cdb4e86f1904ff809adfc7996f174f46b911375e
adithya herath <adithyaidura@gmail.com>
2025-05-19 11:42:11 +0530

    ad hero section

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
-<?php require_once 'includes/header.php'; ?>
+<?php 
+require_once 'includes/header.php'; 
+?>
+
+<!-- Hero Section -->
+<div div class="hero-section my-5">
+</div>
 <?php require_once 'includes/footer.php'; ?>
