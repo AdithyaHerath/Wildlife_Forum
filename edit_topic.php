e67461916b34741927a1deb0a9c5293041b3ec89
Mathee <mathee2004mihi0430@gmail.com>
2025-05-19 09:39:24 +0530

    update edit_topic.php
    Include header, authenticate user, and validate the topic ID parameter

---
+++ edit_topic.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'includes/header.php';
+
+if (!$logged_in) {
+    header("location: login.php");
+    exit();
+}
+
+if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
+    header("location: index.php");
+    exit();
+}
+
+$topic_id = $_GET['id'];
+$error = '';
+$success = '';
+
+
+?>
